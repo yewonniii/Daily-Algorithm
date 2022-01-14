eec59b7b0afed3598afff3fe1f604224a6e6a6c5
iyewon <iyewon@localhost>
2022-01-14 22:04:45 +0900

    BOJ_1065_한수

---
+++ Algorithm/src/BOJ/BOJ_1316.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_1316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt();
+		
+		for (int tc= 0; tc < N; tc++) {
+			String[] arr = new String[]{};
+			
+			for (int i = 0; i < arr.length; i++) {
+				arr[i]=sc.next();
+			}
+			
+			for (int i = 0; i < arr.length-1; i++) {
+				if(arr[i])
+			}
+		}
+
+	}
+
+}
--- Algorithm/src/d0114/BOJ_1065_한수.java
+++ Algorithm/src/d0114/BOJ_1065_한수.java
@@ -1,10 +1,39 @@
 package d0114;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ_1065_한수 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int ans=0;
+		
+		if(N<100) {
+			System.out.println(N);
+		}else {
+			ans=99;
+			
+			if(N==1000) {
+				N=999;
+			}
+			
+			for (int i = 100; i <=N; i++) {
+				int hundred = i/100;
+				int ten = (i/10)%10;
+				int one = i%10;
+				
+				//각 자리가 수열을 이룬다면 
+				if((hundred-ten)==(ten-one)) {
+					ans++;
+				}
+			}
+			System.out.println(ans);
+		}
+		
 	}
 
 }
