ed5df2bf020443bfd5a6ab2077b2b89c2f861fae
yewonniii <lyw0192@naver.com>
2022-01-18 20:16:30 +0900

    프로그래머스_음양더하

---
+++ Algorithm/src/d0118/Programmers_음양더하기.java
@@ -0,0 +1,20 @@
+package d0118;
+
+public class Programmers_음양더하기 {
+	class Solution {
+	    public int solution(int[] absolutes, boolean[] signs) {
+	        int answer=0;
+	        
+	        for(int i=0; i<signs.length; i++){
+	            if(signs[i]) {
+	                answer+=absolutes[i];
+	            }
+	            else{
+	               answer-=absolutes[i];
+	            } 
+	        }
+	        return answer;
+	    }
+	}
+
+}
