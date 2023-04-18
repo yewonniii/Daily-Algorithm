8616b5758a360ad0c8dad6c2d3f8fb3fae1cabf7
yewonniii <lyw0192@naver.com>
2023-04-19 03:01:27 +0900

    BOJ_3985_롤케이크

--- Algorithm/src/2023/04Apr/d0418/BOJ_3985_롤케이크.java
+++ Algorithm/src/2023/04Apr/d0418/BOJ_3985_롤케이크.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_2985_롤케이크 {
+public class BOJ_3985_롤케이크 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
