52251fa1cefbc36cdf1b8c7b676db1c4db948937
yewonniii <lyw0192@naver.com>
2023-04-23 00:19:50 +0900

    프로그래머스_타겟넘버

---
+++ Algorithm/src/2023/04Apr/d0422/프로그래머스_Lv1_타겟넘버.java
@@ -0,0 +1,19 @@
+public class 프로그래머스_Lv1_타겟넘버 {
+    static int result = 0;
+
+    public int solution(int[] numbers, int target) {
+        dfs(0, numbers, target, 0);
+        return result;
+    }
+
+    private static void dfs(int index, int[] num, int target, int sum) {
+        if (index == num.length) {
+            if (target == sum) {
+                result++;
+            }
+            return;
+        }
+        dfs(index + 1, num, target, sum + num[index]);
+        dfs(index + 1, num, target, sum - num[index]);
+    }
+}
